a784839fd24a46ae9df8973bd4b9bbaf9b6c25cf
Per Liedman <per@liedman.net>
2021-06-06 15:09:33 +0200

    Add dropdown

--- src/Courses.js
+++ src/Courses.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useEvent from "./store";
 import shallow from "zustand/shallow";
+import Dropdown, { DropdownItem } from "./ui/Dropdown";
 
 export default function Courses() {
   const { courses, selectedCourseId, setSelected, setName } = useEvent(
@@ -9,29 +10,34 @@ export default function Courses() {
   );
 
   return (
-    <ul>
-      {courses.map((course) => (
-        <li key={course.id} onClick={() => setSelected(course.id)}>
-          <span
-            className={
-              selectedCourseId === course.id
-                ? "text-indigo-600"
-                : "text-gray-darkest"
-            }
-          >
-            {selectedCourseId === course.id ? (
-              <input
-                type="text"
-                value={course.name}
-                onChange={(e) => setName(selectedCourseId, e.target.value)}
-              />
-            ) : (
-              course.name
-            )}
-          </span>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id} onClick={() => setSelected(course.id)}>
+            <span
+              className={
+                selectedCourseId === course.id
+                  ? "text-indigo-600"
+                  : "text-gray-darkest"
+              }
+            >
+              {selectedCourseId === course.id ? (
+                <input
+                  type="text"
+                  value={course.name}
+                  onChange={(e) => setName(selectedCourseId, e.target.value)}
+                />
+              ) : (
+                course.name
+              )}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <Dropdown>
+        <DropdownItem>Load courses...</DropdownItem>
+      </Dropdown>
+    </>
   );
 }
 
---
+++ src/ui/Dropdown.js
@@ -0,0 +1,79 @@
+import React, { createContext, useContext, useState } from "react";
+
+const DropdownContext = createContext();
+
+export default function Dropdown({ children }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <button
+          type="button"
+          className="bg-gray-100 rounded-full flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={() => setOpen(!open)}
+        >
+          <span className="sr-only">Open options</span>
+          <svg
+            className="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+          </svg>
+        </button>
+      </div>
+
+      {/* Dropdown menu, show/hide based on menu state.
+
+    Entering: "transition ease-out duration-100"
+      From: "transform opacity-0 scale-95"
+      To: "transform opacity-100 scale-100"
+    Leaving: "transition ease-in duration-75"
+      From: "transform opacity-100 scale-100"
+      To: "transform opacity-0 scale-95" */}
+      <div
+        className={`transition ease-out duration-100 ${
+          open
+            ? "transform opacity-100 scale-100"
+            : "transform opacity-0 scale-95"
+        }`}
+      >
+        <div
+          className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabindex="-1"
+        >
+          <div className="py-1" role="none">
+            <DropdownContext.Provider value={() => setOpen(false)}>
+              {children}
+            </DropdownContext.Provider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function DropdownItem({ onClick, children }) {
+  const close = useContext(DropdownContext);
+  return (
+    <button
+      className="hover:bg-gray-100 hover:text-gray-900 text-gray-700 block px-4 py-2 text-sm w-full text-left"
+      role="menuitem"
+      tabindex="-1"
+      onClick={(e) => {
+        close();
+        onClick && onClick(e);
+      }}
+    >
+      {children}
+    </button>
+  );
+}
