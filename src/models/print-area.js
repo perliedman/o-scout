cd1daeb39208236e59af4729a7f329c147d9574a
Per Liedman <per@liedman.net>
2021-07-29 17:23:47 +0200

    Add default page dimensions

--- src/models/print-area.js
+++ src/models/print-area.js
@@ -1,10 +1,17 @@
 import { courseBounds } from "./course";
 import { buffer } from "ol/extent";
+import { PAPER_SIZES } from "../services/print";
+
+const {
+  dimensions: [defaultWidth, defaultHeight],
+} = PAPER_SIZES.find((paper) => paper.name === "A4");
 
 export function create(options) {
   return {
     auto: true,
     restrictToPage: true,
+    pageWidth: defaultWidth,
+    pageHeight: defaultHeight,
     ...options,
   };
 }
