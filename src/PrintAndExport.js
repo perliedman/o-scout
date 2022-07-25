5acfda5ce5272c8188282d3b6b186d9cb38b179b
Per Liedman <per@liedman.net>
2022-07-25 16:53:27 +0200

    Add first version of IOF XML export
    Fixes #3.

--- src/PrintAndExport.js
+++ src/PrintAndExport.js
@@ -8,15 +8,19 @@ import Spinner from "./ui/Spinner";
 import { svgToBitmap } from "./services/svg-to-bitmap";
 import * as Course from "./models/course";
 import { transformExtent } from "./services/coordinates";
-import Section from "./ui/Section";
+import { writeIofXml } from "./services/iof-xml";
+import proj4 from "proj4";
 
 export default function PrintAndExport() {
   const [state, setState] = useState("idle");
   const pushNotification = useNotifications(getPush);
-  const { eventName, courseAppearance, courses, selectedCourseId } = useEvent(
-    getCourses,
-    shallow
-  );
+  const event = useEvent();
+  const {
+    name: eventName,
+    courseAppearance,
+    courses,
+    selectedCourseId,
+  } = event;
   const { mapFile, tiler } = useMap(getMap, shallow);
   const selectedCourse = useMemo(
     () => courses.find((course) => course.id === selectedCourseId),
@@ -32,18 +36,18 @@ export default function PrintAndExport() {
   );
 
   useEffect(() => {
-    if (!selectionOptions[selection].enabled(courses)) {
+    if (!selectionOptions[selection].enabled(format, courses)) {
       const nextSelection = Object.keys(selectionOptions).find((option) =>
-        selectionOptions[option].enabled(courses)
+        selectionOptions[option].enabled(format, courses)
       );
       setSelection(nextSelection);
     }
-  }, [courses, selection]);
+  }, [courses, selection, format]);
 
   return (
     <div className="mx-4 pb-4">
       <div className="flex flex-col my-4 text-sm">
-        <div>Print selection</div>
+        <div>Selection</div>
         {Object.keys(selectionOptions).map((option) => (
           <label key={option}>
             <input
@@ -52,13 +56,13 @@ export default function PrintAndExport() {
               name="selection"
               value={option}
               checked={option === selection}
-              disabled={!selectionOptions[option].enabled(courses)}
+              disabled={!selectionOptions[option].enabled(format, courses)}
               onChange={() => setSelection(option)}
             />
             {selectionOptions[option].name}
           </label>
         ))}
-        <div className="mt-2">Print format</div>
+        <div className="mt-2">Format</div>
         {Object.keys(formatOptions).map((option) => (
           <label key={option}>
             <input
@@ -76,7 +80,9 @@ export default function PrintAndExport() {
       <div className="flex justify-end mt-4">
         <Button onClick={onPrint}>
           {state === "printing" && <Spinner className="text-indigo-600" />}
-          {`Print ${selectionOptions[selection].name} to ${formatOptions[format].name}`}
+          {`${format !== "iofXml" ? "Print" : "Export"} ${
+            selectionOptions[selection].name
+          } to ${formatOptions[format].name}`}
         </Button>
       </div>
     </div>
@@ -108,7 +114,28 @@ export default function PrintAndExport() {
         ? (svg) => svgToBitmap(svg)
         : null;
 
-    printNext();
+    if (format !== "iofXml") {
+      printNext();
+    } else {
+      try {
+        const crs = mapFile.getCrs();
+        let projectedToLngLat;
+        if (crs.catalog === "EPSG") {
+          projectedToLngLat = proj4(`EPSG:${crs.code}`, "EPSG:4326").forward;
+        }
+        const doc = writeIofXml(event, crs, projectedToLngLat);
+        const output = new Blob([new XMLSerializer().serializeToString(doc)], {
+          mime: "application/xml",
+        });
+        downloadBlob(output, `${eventName}.xml`);
+        setState("idle");
+      } catch (e) {
+        console.error(e);
+        pushNotification("warning", "Failed to export.", e.toString());
+        setState("error");
+      }
+      return;
+    }
 
     async function printNext() {
       if (printCourses.length === 0) {
@@ -158,18 +185,22 @@ export default function PrintAndExport() {
 }
 
 const selectionOptions = {
-  allCourses: { name: "All Courses", enabled: (courses) => courses.length > 1 },
+  allCourses: {
+    name: "All Courses",
+    enabled: (format, courses) => courses.length > 1,
+  },
   currentCourse: {
     name: "Current Course",
-    enabled: (courses) => courses.length > 0,
+    enabled: (format, courses) => format !== "iofXml" && courses.length > 0,
   },
-  map: { name: "Map", enabled: () => true },
+  map: { name: "Map", enabled: (format) => format !== "iofXml" },
 };
 
 const formatOptions = {
   pdf: { name: "PDF", mime: "application/pdf" },
   svg: { name: "SVG", mime: "image/svg+xml" },
   png: { name: "PNG", mime: "image/png" },
+  iofXml: { name: "IOF 3.0 XML", mime: "application/xml" },
 };
 
 const defaultPrintArea = {
@@ -178,15 +209,6 @@ const defaultPrintArea = {
   pageWidth: 827,
   pageHeight: 1169,
 };
-
-function getCourses({ name, courses, courseAppearance, selectedCourseId }) {
-  return {
-    eventName: name,
-    courses,
-    courseAppearance,
-    selectedCourseId,
-  };
-}
 
 function getMap({ mapFile, tiler }) {
   return { mapFile, tiler };
---
+++ src/services/iof-xml.ts
@@ -0,0 +1,121 @@
+import { Coordinate } from "ol/coordinate";
+import { courseDistance } from "../models/course";
+import { ALL_CONTROLS_ID, Event } from "../models/event";
+import { createXml } from "./xml-utils";
+
+export function writeIofXml(
+  event: Event,
+  crs: OcadCrs,
+  projectedToLngLat: ((c: Coordinate) => Coordinate) | undefined
+): XMLDocument {
+  const doc = document.implementation.createDocument("", "", null);
+  const root = createXml(doc, {
+    type: "CourseData",
+    attrs: {
+      xmlns: "http://www.orienteering.org/datastandard/3.0",
+      "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+      iofVersion: "3.0",
+      createTime: new Date().toISOString(),
+      creator: "O-Scout",
+    },
+    children: [
+      {
+        type: "Event",
+        children: [{ type: "Name", text: event.name }],
+      },
+      {
+        type: "RaceCourseData",
+        children: [...controls(), ...courses()],
+      },
+    ],
+  });
+
+  doc.appendChild(root);
+
+  return doc;
+
+  function controls() {
+    return Object.keys(event.controls).map((controlId) => {
+      const control = event.controls[controlId];
+      const [lng, lat] = projectedToLngLat
+        ? projectedToLngLat(
+            crs.toProjectedCoord(control.coordinates.map((c) => c * 100))
+          )
+        : [undefined, undefined];
+      return {
+        type: "Control",
+        children: [
+          { type: "Id", text: control.code || `_${control.id}` },
+          ...(lng != null
+            ? [
+                {
+                  type: "Position",
+                  attrs: {
+                    lng,
+                    lat,
+                  },
+                },
+              ]
+            : []),
+          {
+            type: "MapPosition",
+            attrs: {
+              unit: "mm",
+              x: control.coordinates[0],
+              y: control.coordinates[1],
+            },
+          },
+        ],
+      };
+    });
+  }
+
+  function courses() {
+    return event.courses
+      .filter((course) => course.id !== ALL_CONTROLS_ID)
+      .map((course) => ({
+        type: "Course",
+        children: [
+          { type: "Name", text: course.name },
+          {
+            type: "Length",
+            text: (courseDistance(course, crs.scale) * 1000).toFixed(0),
+          },
+          ...course.controls.map((control, i) => ({
+            type: "CourseControl",
+            attrs: {
+              type: controlKindToIofType(control.kind),
+            },
+            children: [
+              { type: "Control", text: control.code || `_${control.id}` },
+              {
+                type: "MapText",
+                text: course.labelKind === "sequence" ? i : control.code,
+              },
+            ],
+          })),
+        ],
+      }));
+  }
+}
+
+function controlKindToIofType(kind: string): "Start" | "Finish" | "Control" {
+  switch (kind) {
+    case "start":
+      return "Start";
+    case "finish":
+      return "Finish";
+    case "normal":
+      return "Control";
+    default:
+      throw new Error(`Unhandled control kind ${kind}.`);
+  }
+}
+
+// TODO: incomplete and should ideally be taken of OCAD2GeoJSON
+type OcadCrs = {
+  catalog: string;
+  code: number;
+  scale: number;
+  toProjectedCoord: (c: Coordinate) => Coordinate;
+};
