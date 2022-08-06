e4aec2e7e9e8517055ee298d8a6ea5f402d9460c
Per Liedman <per@liedman.net>
2022-08-06 15:42:04 +0200

    Break out extent interaction

---
+++ src/ol/ExtentInteraction.js
@@ -0,0 +1,115 @@
+import { containsCoordinate, getCenter, getHeight, getWidth } from "ol/extent";
+import OlExtentInteraction from "ol/interaction/Extent";
+
+/**
+ * This is an extended version of OpenLayers' ol/interaction/Extent, with these major differences:
+ *
+ *   * Does not hog all map events: map can still be panned etc.
+ *   * Does only allow to modify the initial extent, not creating a new one by
+ *     clicking outside the map
+ */
+export default class ExtentInteraction extends OlExtentInteraction {
+  /***
+   * @type (eventType: 'extentchangeend', handler: ({type: 'eventchangeend', extent: Extent}) => void)   */
+  on;
+
+  handleEvent(mapBrowserEvent) {
+    super.handleEvent(mapBrowserEvent);
+    return !this.pointerHandler_ && !this.dragDelta_;
+  }
+
+  handlePointerMove_(mapBrowserEvent) {
+    const pixel = mapBrowserEvent.pixel;
+    const map = mapBrowserEvent.map;
+    let cursor = "auto";
+
+    const vertex = this.snapToVertex_(pixel, map);
+    if (vertex) {
+      const extent = this.getExtentInternal();
+      if (vertex[0] === extent[0] && vertex[1] === extent[1]) {
+        cursor = "ne-resize";
+      } else if (vertex[0] === extent[2] && vertex[1] === extent[1]) {
+        cursor = "nw-resize";
+      } else if (vertex[0] === extent[0] && vertex[1] === extent[3]) {
+        cursor = "se-resize";
+      } else if (vertex[0] === extent[2] && vertex[1] === extent[3]) {
+        cursor = "sw-resize";
+      } else if (vertex[0] === extent[0]) {
+        cursor = "w-resize";
+      } else if (vertex[0] === extent[2]) {
+        cursor = "e-resize";
+      } else if (vertex[1] === extent[1]) {
+        cursor = "n-resize";
+      } else {
+        cursor = "s-resize";
+      }
+    } else if (
+      containsCoordinate(this.getExtentInternal(), mapBrowserEvent.coordinate)
+    ) {
+      cursor = "all-scroll";
+    }
+    map.getTarget().style.cursor = cursor;
+  }
+
+  handleDownEvent(mapBrowserEvent) {
+    const pixel = mapBrowserEvent.pixel;
+    const map = mapBrowserEvent.map;
+
+    const extent = this.getExtentInternal();
+    const coordinate = mapBrowserEvent.coordinate;
+
+    const vertex = this.snapToVertex_(pixel, map);
+
+    if (!vertex) {
+      if (containsCoordinate(extent, coordinate)) {
+        const center = getCenter(extent);
+        this.dragDelta_ = [
+          coordinate[0] - center[0],
+          coordinate[1] - center[1],
+        ];
+        this._dragStartExtent = this.extent_;
+        return true;
+      }
+
+      return true;
+    }
+
+    this._dragStartExtent = this.extent_;
+    return super.handleDownEvent(mapBrowserEvent);
+  }
+
+  handleUpEvent(mapBrowserEvent) {
+    this.dragDelta_ = undefined;
+    const handled = super.handleUpEvent(mapBrowserEvent);
+    if (
+      this._dragStartExtent &&
+      this.extent_.some((x, i) => this._dragStartExtent[i] !== x)
+    ) {
+      this.dispatchEvent({ type: "extentchangeend", extent: this.extent_ });
+    }
+
+    return handled;
+  }
+
+  handleDragEvent(mapBrowserEvent) {
+    if (this.dragDelta_) {
+      const coordinate = mapBrowserEvent.coordinate;
+      const center = [
+        coordinate[0] - this.dragDelta_[0],
+        coordinate[1] - this.dragDelta_[1],
+      ];
+      const extent = this.extent_;
+      const width = getWidth(extent);
+      const height = getHeight(extent);
+      this.setExtent([
+        center[0] - width / 2,
+        center[1] - height / 2,
+        center[0] + width / 2,
+        center[1] + height / 2,
+      ]);
+      return true;
+    } else {
+      return super.handleDragEvent(mapBrowserEvent);
+    }
+  }
+}
--- src/tools/PrintArea.js
+++ src/tools/PrintArea.js
@@ -1,6 +1,5 @@
 import Stroke from "ol/style/Stroke";
 import Style from "ol/style/Style";
-import OlExtentInteraction from "ol/interaction/Extent";
 import { useEffect, useMemo, useRef } from "react";
 import { getExtent } from "../models/print-area";
 import {
@@ -10,6 +9,7 @@ import {
 } from "../services/coordinates";
 import useEvent, { useMap } from "../store";
 import shallow from "zustand/shallow";
+import ExtentInteraction from "../ol/ExtentInteraction";
 
 export default function PrintArea() {
   const { map, mapFile } = useMap(getMap);
@@ -76,58 +76,4 @@ function getSelectedCourse({
     course: courses.find(({ id }) => id === selectedCourseId),
     setPrintAreaExtent,
   };
-}
-
-/**
- * This is an extended version of OpenLayers' ol/interaction/Extent, with these major differences:
- *
- *   * Does not hog all map events: map can still be panned etc.
- *   * Does only allow to modify the initial extent, not creating a new one by
- *     clicking outside the map
- */
-class ExtentInteraction extends OlExtentInteraction {
-  handleEvent(mapBrowserEvent) {
-    super.handleEvent(mapBrowserEvent);
-    return !this.pointerHandler_;
-  }
-
-  handlePointerMove_(mapBrowserEvent) {
-    const pixel = mapBrowserEvent.pixel;
-    const map = mapBrowserEvent.map;
-    let cursor = "auto";
-
-    const vertex = this.snapToVertex_(pixel, map);
-    if (vertex) {
-      const extent = this.getExtentInternal();
-      if (vertex[0] === extent[0] && vertex[1] === extent[1]) {
-        cursor = "ne-resize";
-      } else if (vertex[0] === extent[2] && vertex[1] === extent[1]) {
-        cursor = "nw-resize";
-      } else if (vertex[0] === extent[0] && vertex[1] === extent[3]) {
-        cursor = "se-resize";
-      } else if (vertex[0] === extent[2] && vertex[1] === extent[3]) {
-        cursor = "sw-resize";
-      } else if (vertex[0] === extent[0]) {
-        cursor = "w-resize";
-      } else if (vertex[0] === extent[2]) {
-        cursor = "e-resize";
-      } else if (vertex[1] === extent[1]) {
-        cursor = "n-resize";
-      } else {
-        cursor = "s-resize";
-      }
-    }
-    map.getTarget().style.cursor = cursor;
-  }
-
-  handleDownEvent(mapBrowserEvent) {
-    const pixel = mapBrowserEvent.pixel;
-    const map = mapBrowserEvent.map;
-
-    const vertex = this.snapToVertex_(pixel, map);
-
-    if (!vertex) return true;
-
-    return super.handleDownEvent(mapBrowserEvent);
-  }
 }
