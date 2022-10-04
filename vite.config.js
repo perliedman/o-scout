f7b84f61e2127b8bb5ce077c6ab31fb59fff89f9
Joakim Berglund <neorth@gmail.com>
2022-10-04 22:07:54 +0200

    Package updates and cofig changes

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+});
