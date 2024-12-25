734e52848db3d5c30532a0c699e8df2f2fac01a2
wlfrdssn <61554783+wlfrdssn@users.noreply.github.com>
2024-12-25 16:18:21 +0100

    Update PrintArea.js
    Added that the print area turns red if it is drawn larger than the paper size and also added the paper size to be visible on the map.

--- src/tools/PrintArea.js
+++ src/tools/PrintArea.js
@@ -10,6 +10,12 @@ import useEvent, { useMap } from "../store";
 import shallow from "zustand/shallow";
 import ExtentInteraction from "../ol/ExtentInteraction";
 import { getPrintAreaExtent } from "../models/course";
+import { printCourse } from "../services/print";
+
+import Feature from 'ol/Feature.js';
+import {Vector as VectorLayer} from 'ol/layer.js';
+import {Vector as VectorSource} from 'ol/source.js';
+import {fromExtent} from 'ol/geom/Polygon.js';
 
 export default function PrintArea() {
   const { map, mapFile } = useMap(getMap);
@@ -17,29 +23,78 @@ export default function PrintArea() {
 
   const crs = useMemo(() => mapFile?.getCrs(), [mapFile]);
 
+
   useEffect(() => {
     if (map && course) {
       const initialExtent = transformExtent(
         getPrintAreaExtent(course, crs.scale),
         (c) => toProjectedCoord(crs, c)
       );
+      
+      const PrintAreaExtentI = getPrintAreaExtent(course)
+      const check_height = (course.printArea.pageHeight/3.937)-((PrintAreaExtentI[3]-PrintAreaExtentI[1])*(15000/course.printScale))
+      const check_width = (course.printArea.pageWidth/3.937)-((PrintAreaExtentI[2]-PrintAreaExtentI[0])*(15000/course.printScale))
+    
+
+      let boxStyle;
+      
+      if (check_height < 0 || check_width < 0) {
+        boxStyle = new Style({
+          stroke: new Stroke({ color: "#FF0000", lineDash: [6, 10], width: 3 }),
+          fill: null,
+        });
+      } else {
+        boxStyle = new Style({
+          stroke: new Stroke({ color: "#444", lineDash: [6, 10], width: 3 }),
+          fill: null,
+        });
+      }
+      
       const interaction = new ExtentInteraction({
         extent: initialExtent,
         boxStyle,
         pointerStyle: new Style(),
       });
+
+      const rectCoords = []
+
+      
+      //Papersize on map. 15000 mapscale??
+      rectCoords[0] = initialExtent[0] 
+      rectCoords[1] = initialExtent[1] 
+      rectCoords[2] = initialExtent[0]+((course.printArea.pageWidth)*(course.printScale/15000)*(96*(1/25.4)))
+      rectCoords[3] = initialExtent[1]+((course.printArea.pageHeight)*(course.printScale/15000)*(96*(1/25.4)))
+  
+
+      const printLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [
+            new Feature(
+              fromExtent(rectCoords),
+            ),
+          ],
+        }),
+      });
+      
+
+  
       interaction.on("extentchangeend", ({ extent }) => {
+        
         if (extent) {
           setPrintAreaExtent(
             course.id,
             transformExtent(extent, (c) => fromProjectedCoord(crs, c))
           );
+         
+          
         }
       });
       map.addInteraction(interaction);
+      map.addLayer(printLayer);
 
       return () => {
         map.removeInteraction(interaction);
+        map.removeLayer(printLayer);
       };
     }
   }, [crs, mapFile, map, course, setPrintAreaExtent]);
@@ -47,10 +102,7 @@ export default function PrintArea() {
   return null;
 }
 
-const boxStyle = new Style({
-  stroke: new Stroke({ color: "#444", lineDash: [6, 10], width: 3 }),
-  fill: null,
-});
+
 
 function getMap({ map, mapFile }) {
   return { map, mapFile };
