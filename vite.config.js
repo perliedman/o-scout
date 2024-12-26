9d4d40fc73b099f752a43c2305f6c3d8d1d9e9aa
Per Liedman <per@liedman.net>
2024-12-26 16:55:26 +0100

    Fix build mode node deps
    Use vite-plugin-node-polyfills even though its peer deps says it doesn't support vite 6...

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
